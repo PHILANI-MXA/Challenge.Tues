43a3159d276e889d66201b32420e30aae453b986
PHILANI-MXA <3911755@myuwc.ac.za>
2022-06-21 09:54:21 +0200

    initial commit

---
+++ script/script.js
@@ -0,0 +1,22 @@
+let lists = JSON.parse(localStorage.getItem('records')) ?
+    JSON.parse(localStorage.getItem('records')) : [{
+        name: 'Lerato',
+        rgb: '',
+
+    }];
+
+function addData() {
+    // e.preventDefault();
+    lists.push({
+        name: document.getElementById('name').value,
+        rgb: document.getElementById('color').value,
+
+    });
+    // Save data to a localstorage
+    localStorage.setItem('records', JSON.stringify(lists));
+}
+document.querySelector('#addRecord').addEventListener('click', addData);
+// Displaying data
+(function loadData() {
+    console.table(lists);
+})();
